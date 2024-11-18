935309b1a321f340f78d26ccf41f209f24c25cee
JuanLC28 <juan.50.cristobal@gmail.com>
2024-11-17 23:56:45 -0500

    Detalles para publicarlo en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://juanlc28.github.io',
-  base: 'portfolio-web',
+  base: '/portfolio-web/',
 });
